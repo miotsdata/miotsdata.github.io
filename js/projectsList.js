3e9fda0331bbb700403af07c8b20d6dc6cb9b6de
Matteo Miotto pro <miotsdata@gmail.com>
2025-05-15 19:58:44 +0200

    update projects links

--- js/projectsList.js
+++ js/projectsList.js
@@ -1,59 +1,30 @@
 const projectsSource = [
     {
         "title": "R for biologist",
-        "url": "https://mmiots9.github.io/R-for-biologist",
+        "url": "https://miotsdata.github.io/R-for-biologist",
         "img": "images/R-for-biologist.png",
         "alt": "R for biologist project image",
         "description": "In this book I explain how to use R for biologist, in a practical and easy-to-follow manner.",
     },
     {
         "title": "Pyabnotebook",
-        "url": "https://github.com/mmiots9/pylabnotebook",
+        "url": "https://github.com/miotsdata/pylabnotebook",
         "img": "images/labnotebook.png",
         "alt": "Labnotebook project image",
         "description": "This project (python <u><a href='https://pypi.org/project/pylabnotebook/' target='_blank'>package</a></u>) aims to help bioinformaticians in creating the so called <em>Laboratory notebook</em> in an automatic way.",
     },
     {
         "title": "Startnewproject",
-        "url": "https://github.com/mmiots9/startnewproject",
+        "url": "https://github.com/miotsdata/startnewproject",
         "img": "images/startnewproject.png",
         "alt": "Startnewproject project image",
         "description": "This project (python <u><a href='https://pypi.org/project/startnewproject/' target='_blank'>package</a></u>) helps by creating a new folder for a project with specified subfolders, README files, git and pylabnotebook initialized.",
     },
     {
         "title": "Bash functions",
-        "url": "https://github.com/mmiots9/bash-functions",
+        "url": "https://github.com/miotsdata/bash-functions",
         "img": "images/bash.png",
         "alt": "Bash functions project image",
         "description": "Here, I collect useful bash functions I use to work.",
-    },
-    {
-        "title": "Mdarts",
-        "url": "https://github.com/mmiots9/mdarts",
-        "img": "images/mdarts.png",
-        "alt": "Mdarts project image",
-        "description": "This is one of my first projects, in which I wanted to create an R package for those like me who love playing darts. Here you can find functions for all main darts games.",
-    },
-    {
-        "title": "Lodutilities",
-        "url": "https://github.com/mmiots9/lodutilities",
-        "img": "images/lodutilities.png",
-        "alt": "Lodutilities project image",
-        "description": "Here you can find useful functions I created for my job in the Neurodevelopment lab in Humanitas.",
-    },
-    
-    {
-        "title": "Darts app",
-        "url": "https://github.com/mmiots9/Darts_app",
-        "img": "images/dartboard.png",
-        "alt": "Darts app project image",
-        "description": "This web app is designed for darts players who want to easily play various games and take advantages of automatic score calculation.",
-    },
-    {
-        "title": "Irregular verbs",
-        "url": "https://github.com/mmiots9/irregular-verbs",
-        "img": "images/irregular-verbs.png",
-        "alt": "Irregular verbs project image",
-        "description": "This Flask app was designed to help my mother studying english irregular verbs.",
     },
 ]
